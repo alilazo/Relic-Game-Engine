634730f297626e97f379898588843d5340bd39aa
alilazo <lazoali98@gmail.com>
2023-03-19 23:59:29 -0500

    Camera Fix

--- Relic/include/InputHandler.cpp
+++ Relic/include/InputHandler.cpp
@@ -1,64 +1,82 @@
+#define _USE_MATH_DEFINES
+
+
 #include <SFML/Graphics.hpp>
+#include <cmath>
 #include <iostream>
 #include <string>
 
 class InputHandler {
+private:
+    sf::Sprite *player;
 public:
-    void setHandler(std::string setting, sf::CircleShape& shape, sf::Event event);
+    InputHandler(sf::Sprite *p) { player = p;}
+    void setMovementHandler(std::string setting, sf::Event event);
+    void setPlayerPositionToMouse(sf::RenderWindow &window);
+    sf::Sprite *getPlayer(){return player; };
 };
 
-void InputHandler::setHandler(std::string setting, sf::CircleShape &player, sf::Event event){
+//TODO: Mouse Origin is fixed at (0,0) needs to be fixed at player.
+void InputHandler::setPlayerPositionToMouse(sf::RenderWindow &window){
+    // Get the position of the mouse on the screen
+    sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+    // Calculate the angle between the position of the mouse and the position of the player
+    sf::Vector2f playerPos = player->getPosition();
+
+    float dx = mousePos.x;
+    float dy = mousePos.y;
+    float angle = atan2(dy, dx) * 180 / M_PI;
+
+    // Rotate the player sprite to that angle
+    player->setRotation(angle);
+}
+
+void InputHandler::setMovementHandler(std::string setting, sf::Event event){
     if(setting == "arrows"){
         if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x + 20, player.getPosition().y);
+            player->setPosition(player->getPosition().x + 20, player->getPosition().y);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x, player.getPosition().y + 20);
+            player->setPosition(player->getPosition().x, player->getPosition().y + 20);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x - 20, player.getPosition().y);
+            player->setPosition(player->getPosition().x - 20, player->getPosition().y);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x, player.getPosition().y - 20);
+            player->setPosition(player->getPosition().x, player->getPosition().y - 20);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
         {
-            std::cout << "Player Debug: X: " << player.getPosition().x << " Y: " << player.getPosition().y << std::endl;
+            std::cout << "Player Debug: X: " << player->getPosition().x << " Y: " << player->getPosition().y << std::endl;
         }
     } else if (setting == "keyboard"){
         if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::D)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x + 20, player.getPosition().y);
+            player->setPosition(player->getPosition().x + 20, player->getPosition().y);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::S)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x, player.getPosition().y + 20);
+            player->setPosition(player->getPosition().x, player->getPosition().y + 20);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::A)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x - 20, player.getPosition().y);
+            player->setPosition(player->getPosition().x - 20, player->getPosition().y);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::W)
         {
-            player.setFillColor(sf::Color::Red);
-            player.setPosition(player.getPosition().x, player.getPosition().y - 20);
+            player->setPosition(player->getPosition().x, player->getPosition().y - 20);
         }
         else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
         {
-            std::cout << "Player Debug: X: " << player.getPosition().x << " Y: " << player.getPosition().y << std::endl;
+            std::cout << "Player Debug: X: " << player->getPosition().x << " Y: " << player->getPosition().y << std::endl;
         }
     }
+
+    //setPlayerPositionToMouse(window);
 }
 
 /*
@@ -66,27 +84,27 @@ void InputHandler::setHandler(std::string setting, sf::CircleShape &player, sf::
 
 if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
 {
-    player.setFillColor(sf::Color::Red);
-    player.setPosition(player.getPosition().x + 20, player.getPosition().y);
+    player->setFillColor(sf::Color::Red);
+    player->setPosition(player->getPosition().x + 20, player->getPosition().y);
 }
 else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down)
 {
-    player.setFillColor(sf::Color::Red);
-    player.setPosition(player.getPosition().x, player.getPosition().y + 20);
+    player->setFillColor(sf::Color::Red);
+    player->setPosition(player->getPosition().x, player->getPosition().y + 20);
 }
 else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
 {
-    player.setFillColor(sf::Color::Red);
-    player.setPosition(player.getPosition().x - 20, player.getPosition().y);
+    player->setFillColor(sf::Color::Red);
+    player->setPosition(player->getPosition().x - 20, player->getPosition().y);
 }
 else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up)
 {
-    player.setFillColor(sf::Color::Red);
-    player.setPosition(player.getPosition().x, player.getPosition().y - 20);
+    player->setFillColor(sf::Color::Red);
+    player->setPosition(player->getPosition().x, player->getPosition().y - 20);
 }
 else if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
 {
-    std::cout << "Player Debug: X: " << player.getPosition().x << " Y: " << player.getPosition().y << std::endl;
+    std::cout << "Player Debug: X: " << player->getPosition().x << " Y: " << player->getPosition().y << std::endl;
 }
 
 
--- Relic/main.cpp
+++ Relic/main.cpp
@@ -3,6 +3,7 @@
 #include <InputHandler.cpp>
 #include <iostream>
 
+//Use call by reference instead of value
 void initView(sf::View& view, sf::FloatRect visibleArea, sf::FloatRect backgroundBounds, sf::Vector2f playerPos, float playerRadius) {
     view.setViewport(sf::FloatRect(0.f, 0.f, 1.f, 1.0f));
     // Restrict the view to the visible area of the background sprite
@@ -29,29 +30,36 @@ int main()
     sf::Texture bgTexture;
     if(!bgTexture.loadFromFile("Resources/bg.jpg")){ return EXIT_FAILURE; }
 
+    //Create the player Texture
+    sf::Texture playerTexture;
+    if(!playerTexture.loadFromFile("Resources/player.png")){ return EXIT_FAILURE; }
+
     //Create a sprite to hold the background texture
     sf::Sprite bgSprite(bgTexture);
     bgSprite.setScale(2.0f, 2.0f);
+
+    //Create a player sprite
+    //TODO: Create a "Hitbox" for the player also initilize size of the players dimensions.
+    sf::Sprite player(playerTexture);
+    player.setScale(0.3f, 0.3f);
+    player.setPosition(120.f, 120.f);
 
     //Get the bounds from background
     sf::FloatRect bgBounds = bgSprite.getLocalBounds();
     std::cout << "Background Bounds: Top: " << bgBounds.top << " Width: " << bgBounds.width << " Height: " << bgBounds.height << " Left: " << bgBounds.left << std::endl;
 
-    //Create the player
-    sf::CircleShape player(20.0f);
-    player.setFillColor(sf::Color::Green);
-    player.setPosition(100.0f, 100.0f);
-
     //Create the Enemy
     Enemy newEnemy(20.f);
     newEnemy.setPosition(120.f, 120.f);
 
+
     //Create a view that matches the size of the window
     sf::View view(sf::FloatRect(0, 0, 900, 500));
-    initView(view, view.getViewport(), bgBounds, player.getPosition(), player.getRadius());
+    initView(view, view.getViewport(), bgBounds, player.getPosition(), 20.f);
 
     //Init the InputHandler
-    InputHandler defaultInput;
+    InputHandler gameInput(&player);
+    //gameInput.setPlayer(player);
 
     std::cout << "Finished init." << std::endl;
     while (window.isOpen())
@@ -62,12 +70,13 @@ int main()
             if (event.type == sf::Event::Closed){
                 window.close();
             }
-            defaultInput.setHandler("arrows", player, event);
+            gameInput.setMovementHandler("keyboard", event);
         }
+
+        gameInput.setPlayerPositionToMouse(window);
 
         //Get current pos of circle
         sf::Vector2f circlePos = player.getPosition();
-        float circleRadius = player.getRadius();
 
         //Make the viewport stay in the bounds of the Background
         //Make function that has offsets to integrate it to the camera Class.
@@ -86,10 +95,10 @@ int main()
 
         //TODO: Restrict the view to the background bounds.
         sf::FloatRect visibleArea(sf::Vector2f(view.getSize().x, view.getSize().y), view.getSize());
-        initView(view, visibleArea, bgBounds, circlePos, circleRadius);
+        initView(view, visibleArea, bgBounds, circlePos, 20.f);
 
         //Update the view to the center on the circle with offset of radius
-        view.setCenter(circlePos.x + player.getRadius(), circlePos.y + player.getRadius());
+        view.setCenter(circlePos.x + 20.f, circlePos.y + 20.f);
 
 
         window.setView(view);
@@ -99,7 +108,6 @@ int main()
         window.draw(bgSprite);
         window.draw(newEnemy.drawEnemy());
         window.draw(player);
-
         window.display();
     }
 
